d7c1bea14648e7052f61106b345b84d180187a4d
Ersoy TORAMAN <jovannijares@gmail.com>
2019-05-29 16:24:25 +0300

    autherize test page added.

--- API/API/Controllers/UsersController.cs
+++ API/API/Controllers/UsersController.cs
@@ -2,41 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    [Authorize]
+    public class UsersController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Users
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
-            return new string[] { "changed 1", "changed 2" };
+            return new string[] { "user 1", "user 2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
-            return "value";
+            return "user " + id;
         }
 
-        // POST api/values
+        // POST: api/Users
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Users/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
